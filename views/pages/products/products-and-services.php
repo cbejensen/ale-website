df5df2f04907fa2e8d862a736dc65a40a9203851
Jack Brown <jack@atlanticlabequipment.com>
2017-03-07 10:51:22 -0500

    Added products and services index page

---
+++ views/pages/products/products-and-services.php
@@ -0,0 +1,62 @@
+<?php 
+/*
+ * This is the index of the Products and Services section of the site.
+ * It should contain prompts and links to each of the subsections, which
+ * are also accessible by the top nav menu.
+ * 
+ * Primarily, the Premium Automation solutions will be advertized,
+ * but there should be a number of listings/ads on the page as well,
+ * preferably above the fold, if possible.
+ * 
+ * Jack Brown
+ * March 7, 2017
+ */
+?>
+<main class="products-main">
+	<h1>Products &amp; Services</h1>
+	<section class="auto-solutions-section">
+		<div class="auto-solutions-wrap">
+			<h2>Premium Automation Solutions</h2>
+			<div class="auto-solutions-column">
+				<img src="" alt="">
+				<h3>Integrated Automation</h3>
+				<p>Integrating devices to your liquid handling platform can 
+				improve	the process flow and overall throughput in your lab.
+				ALE has experience integrating devices and confidence in our
+				ability to customize a solution for your needs.</p>
+				<a href="">Learn More</a>
+			</div>
+			<div class="auto-solutions-column">
+				<img src="" alt="">
+				<h3>Installation &amp; Training</h3>
+				<p>Installation and training will help you deliver timely
+				results. Following the manufacturer’s installation procedures
+				is imperative for the overall performance of your systems.
+				ALE provides top-quality installation and training, addressing
+				both regulatory and safety concerns.</p>
+				<a href="">Learn More</a>
+			</div>
+			<div class="auto-solutions-column">
+				<img src="" alt="">
+				<h3>Application Development &amp; Service</h3>
+				<p>Providing a total solution from pre-sales evaluation to post-sales
+				support is critical for the success of your lab. The team at ALE is
+				capable of providing first-rate application method templates, hardware,
+				software technical support and service contracts.</p>
+				<a href="">Learn More</a>
+			</div>
+		</div>
+	</section>
+	<section class="premium-equipment-section">
+	</section>
+	<section class="hve-ads-section">
+	</section>
+	<section class="new-equipment-section">
+	</section>
+	<section class="monthly-specials-section">
+	</section>
+	<section class="waters-equipment-section">
+	</section>
+	<section class="outlet-ad-section">
+	</section>
+</main>
