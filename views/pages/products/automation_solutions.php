420315162029decc7f93895372589e0a7e5afd9d
Jack Brown <jack@atlanticlabequipment.com>
2017-03-10 15:16:47 -0500

    Added Premium Automation Solutions page and markup. Added Monthly Specials, New Arrivals, and Waters Equipment pages, but no markup. The content of the latter pages will mostly be generated dynamically via the database.

---
+++ views/pages/products/automation_solutions.php
@@ -0,0 +1,59 @@
+<?php
+/*
+ * This is the Premium Automation Solutions page, under the Products & Services section.
+ * This page contains information about our flagship service: the development of
+ * automated, integrated systems for specific applications.
+ */
+?>
+<main class="automation-solutions-main">
+	<header>
+		<h1>Premium Automation Solutions</h1>
+		<p>Atlantic Lab Equipment's dynamic product mix, focused on liquid handling,
+		is able to provide premium solutions for a variety of applications in the Life
+		Science market. ALE's core technical team has successfully implemented variations
+		of these applications in labs in industry, government, and academia.</p>
+	</header>
+	<section class="automation-solutions-section">
+		<!-- This section contains a video on the current site. Add it? -->
+		<h1>Integrated Automation</h1>
+		<p>ALE is cabable of integrating multiple devices to the Tecan liquid handling
+		robotic system, providing a fully automated solution to the customer.</p>
+		<p>To date, ALE has integrated:</p>
+		<ul>
+			<li>Centrifuges</li>
+			<li>Plate Readers</li>
+			<li>Plate Washers</li>
+			<li>Heating &amp; Cooling Devices</li>
+			<li>Plate Sealers</li>
+			<li>Plate Storage Units</li>
+		</ul>
+		<p>We also have the capability of developing and implementing other
+		automation-friendly devices to the Tecan platform if desired, thus
+		improving the process flow and throughput in the lab.</p>
+	</section>	
+	<section class="automation-solutions-section">
+		<!-- Historically, this section has not featured any visual elements, but some should be added -->
+		<h1>Installation &amp; Training</h1>
+		<p>ALE is able to provide the installation and training of Tecan systems per
+		the manufacturer’s documentation. Typically, three days are itemized with the
+		sale of an automated solution enabling a template method developed for the
+		customer along with training.</p>
+		<p>We are also able to customize the training based on the requirements in the
+		lab. Our trained team of engineers have considerable experience training lab 
+		technicians and scientists.</p>
+	</section>
+	<section class="automation-solutions-section">
+		<!-- This section also contains a video on the current site, so it should be added, as long as the
+		quality is acceptable. -->
+		<h1>Application Development &amp; Service</h1>
+		<p>Having considerable experience with Tecan automation, ALE is capable of
+		providing application development services to our customers who desire it.</p>
+		<p>Our trained staff’s objective is to provide quality templates in a timely
+		manner, thus reducing time to first result for the lab. Our engineering team
+		works both remotely and on-site to resolve hardware and software-related issues 
+		while also offering full service contracts and Preventive Maintenance (PM)
+		agreements for the Tecan solutions.</p>
+		<p>With staff located throughout the US, our responses are timely, 
+		thus down time with our instruments is very low.</p>
+	<section>
+</main>
