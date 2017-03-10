a024291b8c0a3438c2796ba12b6c862fc1ba9585
Jack Brown <jack@atlanticlabequipment.com>
2017-03-10 11:34:06 -0500

    Added markup to the Products & Services index, including calls to currently-undefined functions that will dynamically insert our equipment 'ads' into the appropriate sections.

--- forms/estimate-form.php
+++ forms/estimate-form.php
@@ -1,23 +1,23 @@
 <?php
 if($_POST["instrument"]) {
-	$recipient="jack@atlanticlabequipment.com";
-	$ale="Atlantic Lab Equipment";
+	$recipient	=	"jack@atlanticlabequipment.com";
+	$ale		=	"Atlantic Lab Equipment";
 	
-	$sender=$_POST["firstname"];
-	$sender2=$_POST["lastname"];
-	$senderEmail=$_POST["email"];
-	$phone=$_POST["phone"];
-	$word=$_POST["word"];
-	$comments=$_POST["comments"];
-	$instrument=$_POST["instrument"];
-	$infoRequest=$_POST["info-requested"];
-	$mailing=$_POST["mailing"];
+	$sender			=	$_POST["firstname"];
+	$sender2		=	$_POST["lastname"];
+	$senderEmail	=	$_POST["email"];
+	$phone			=	$_POST["phone"];
+	$word			=	$_POST["word"];
+	$comments		=	$_POST["comments"];
+	$instrument		=	$_POST["instrument"];
+	$infoRequest	=	$_POST["info-requested"];
+	$mailing		=	$_POST["mailing"];
 	
-	$subject="$sender $sender2 asked for a quote!";
-	$mailbody="Name: $sender $sender2\nEmail: $senderEmail\nPhone: $phone\n\nInstrument of Interest: $instrument\nInformation Requested: $infoRequest\n\nAdditional Questions or Comments: $comments\n\nSigned up for Newsletter? $mailing\nHow did you hear about us? $word";
+	$subject	=	"$sender $sender2 asked for a quote!";
+	$mailbody	=	"Name: $sender $sender2\nEmail: $senderEmail\nPhone: $phone\n\nInstrument of Interest: $instrument\nInformation Requested: $infoRequest\n\nAdditional Questions or Comments: $comments\n\nSigned up for Newsletter? $mailing\nHow did you hear about us? $word";
 	
-	$subject2="Thank You from ALE! - Submission Confirmation";
-	$mailbody2="Hello $sender,\n\nThank you for your interest in Atlantic Lab Equipment! We'll contact you with a quote as soon as possible.";
+	$subject2	=	"Thank You from ALE! - Submission Confirmation";
+	$mailbody2	=	"Hello $sender,\n\nThank you for your interest in Atlantic Lab Equipment! We'll contact you with a quote as soon as possible.";
 	
 	mail($recipient, $subject, $mailbody, "From: $sender $sender2<$senderEmail>");
 	mail($senderEmail, $subject2, $mailbody2, "From: $ale <$recipient>");	
--- views/pages/products/index.php
+++ views/pages/products/index.php
@@ -4,9 +4,8 @@
  * It should contain prompts and links to each of the subsections, which
  * are also accessible by the top nav menu.
  * 
- * Primarily, the Premium Automation solutions will be advertized,
- * but there should be a number of listings/ads on the page as well,
- * preferably above the fold, if possible.
+ * The Premium Automation solutions have the top spot,
+ * but there should be a number of listings/ads on the page as well.
  * 
  * Jack Brown
  * March 7, 2017
@@ -16,10 +15,10 @@
 	<h1>Products &amp; Services</h1>
 	<section class="auto-solutions-section">
 		<div class="auto-solutions-wrap">
-			<h2>Premium Automation Solutions</h2>
+			<h1>Premium Automation Solutions</h1>
 			<div class="auto-solutions-column">
 				<img src="" alt="">
-				<h3>Integrated Automation</h3>
+				<h2>Integrated Automation</h2>
 				<p>Integrating devices to your liquid handling platform can 
 				improve	the process flow and overall throughput in your lab.
 				ALE has experience integrating devices and confidence in our
@@ -28,7 +27,7 @@
 			</div>
 			<div class="auto-solutions-column">
 				<img src="" alt="">
-				<h3>Installation &amp; Training</h3>
+				<h2>Installation &amp; Training</h2>
 				<p>Installation and training will help you deliver timely
 				results. Following the manufacturer’s installation procedures
 				is imperative for the overall performance of your systems.
@@ -38,7 +37,7 @@
 			</div>
 			<div class="auto-solutions-column">
 				<img src="" alt="">
-				<h3>Application Development &amp; Service</h3>
+				<h2>Application Development &amp; Service</h2>
 				<p>Providing a total solution from pre-sales evaluation to post-sales
 				support is critical for the success of your lab. The team at ALE is
 				capable of providing first-rate application method templates, hardware,
@@ -48,15 +47,48 @@
 		</div>
 	</section>
 	<section class="premium-equipment-section">
+		<img src="" alt="">
+		<h1>Premium Equipment</h1>
+		<span>ALE provides tested, reconditioned, and refurbished equipment: reliable products
+		backed by outstanding service. <!-- Link prior stmt to testimonials? --></span>
+		<span>Our extensive, premium inventory includes:</span>
+		<ul>
+			<li>Liquid handlers</li>
+			<li>Laboratory automation instruments</li>
+			<li>HPLC, FPLC, GC, and Mass Spectrometry Analytical Systems</li>
+			<li>Thermal cyclers</li>
+			<li>PCR systems</li>
+			<li>DNA sequencers</li>
+			<li>Imaging Systems</li>
+			<li>Microplate readers &amp; washers</li>
+		</ul>
+		<a href="">View Catalog</a>
 	</section>
 	<section class="hve-ads-section">
+		<h1>Featured Premium Equipment</h1>
+		<?php //getFeaturedEquipment(); ?>
 	</section>
 	<section class="new-equipment-section">
+		<h1>New Arrivals</h1>
+		<?php //getNewArrivals(); ?>
 	</section>
 	<section class="monthly-specials-section">
+		<h1>Monthly Equipment Specials</h1>
+		<?php //getMontlySpecials(); ?>
 	</section>
 	<section class="waters-equipment-section">
+		<h1>WATERS Analytical Equipment</h1>
+		<?php //getWatersEquipment(); ?>
 	</section>
 	<section class="outlet-ad-section">
+		<img src="" alt="">
+		<h1>Outlet Equipment</h1>
+		<span>As-Is instrumentation on our eBay store.</span>
+		<p>We specialize in pre-owned, biotech, pharmaceutical,
+		and basic research laboratory equipment. Many of our listings
+		feature fully tested, functional equipment, and we offer units
+		"as-is" and "for parts" at discounted prices. We also carry
+		lab supplies and consumables.</p>
+		<a href="">View Catalog</a>
 	</section>
 </main>
