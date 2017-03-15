12f800db838f06dea9a7b20a2c54f8119bf4bc68
Jack Brown <jack@atlanticlabequipment.com>
2017-03-15 17:33:35 -0400

    Moved and updated the routes file. Added to the list of valid controllers and actions. Noted that the user should be validated if they're requesting an admin page, and that validation should occur before the requested method is called.

---
+++ resources/routes.php
@@ -0,0 +1,63 @@
+<?php
+
+// list of controllers and their actions -- allowed values
+$controllers	= array('webpages' 			=>	[	'home', 	'products_services', 
+													'contact', 	'estimates', 
+													'error', 	'test'
+													],
+		
+						'admin_pages'		=> 	[	'dashboard',	'manage_ads'
+													],
+		
+						'admin_inventory'	=> 	[	'addManufacturer'
+													]
+						//''			=> ['', '']
+);
+
+// check that requested controller and action are allowed
+if (array_key_exists($controller, $controllers))
+{
+	(in_array($action, $controllers[$controller])) ? call($controller, $action) : call('pages', 'error');
+}
+else
+{
+	call('pages', 'error');
+}
+// End of script
+
+
+
+function call($controller, $action)
+{
+	require_once LIB_PATH . '/controllers/' . $controller . '_controller.php';
+
+	// create a new instance of the needed controller
+	switch ($controller)
+	{
+		case 'webpages':
+			$controller	= new WebpagesController();
+			break;
+		case 'admin_pages':
+			// Validate user before proceeding
+			$controller = new Admin_PagesController();
+			break;
+		case 'admin_inventory':
+			// Validate user before proceeding
+			require_once 'models/inventory_model.php';
+			$controller = new InventoryController();
+			break;
+		case 'admin_lists':
+			// Validate user before proceeding
+			//require_once '';
+			//$controller = new ListsController();
+			//break;
+		case 'admin_qb':
+			// Validate user before proceeding
+			//require_once '';
+			//$controller = new QbController();
+			//break;
+	}
+
+	// call the action
+	$controller->{ $action }();
+}
