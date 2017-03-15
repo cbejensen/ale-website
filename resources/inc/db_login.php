fee99284909c015f781d1d31b7a4eb7158d180ca
Jack Brown <jack@atlanticlabequipment.com>
2017-03-15 17:16:34 -0400

    Moved db login files

---
+++ resources/inc/db_login.php
@@ -0,0 +1,5 @@
+<?php //db_login.php
+$hn	=	'localhost';
+$db	=	'al_db';
+$un	=	'jaczzk'; // guest
+$pw	=	'thisisthedb'; // default_ale_guest
---
+++ resources/inc/db_login_novartis.php
@@ -0,0 +1,5 @@
+<?php //db_login_novartis.php
+$hn		=	'localhost';
+$db		=	''; // Novartis Database
+//$un	=	'guest';
+//$pw	=	'default_ale_guest';
