f4a10421095bdc0e8bf646a099b46a7c4bffa5ef
Jack Brown <jack@atlanticlabequipment.com>
2017-03-15 17:26:52 -0400

    Deleted old directories and files as part of a refactoring. The new structure will be described in resources/README.md

--- inc/db_login.php
+++
@@ -1,5 +0,0 @@
-<?php //db_login.php
-$hn	=	'localhost';
-$db	=	'al_db';
-$un	=	'jaczzk'; // guest
-$pw	=	'thisisthedb'; // default_ale_guest
--- inc/db_login_novartis.php
+++
@@ -1,5 +0,0 @@
-<?php //db_login_novartis.php
-$hn		=	'localhost';
-$db		=	''; // Novartis Database
-//$un	=	'guest';
-//$pw	=	'default_ale_guest';
