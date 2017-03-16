d29eb50adf020e66a627e0a3d40f6df6e8ae81f6
Jack Brown <jack@atlanticlabequipment.com>
2017-03-16 17:08:09 -0400

    Updated db_connect function to accept user data argument as an array. Added db_query function, which will alert the user if a query has failed and log the error in a log. These alert and logging actions are performed by functions which were also added to this script.

--- resources/lib/functions.php
+++ resources/lib/functions.php
@@ -1,5 +1,4 @@
 <?php 
-
 function db_connect($database, $userData)
 {
 	switch ($database)
@@ -14,30 +13,71 @@ function db_connect($database, $userData)
 	$conn = new mysqli($hn, $un, $pw, $db);
 	if ($conn->connect_error)
 	{
-		ini_set('error_log', 'logs/mysql-errors.log');
-		error_log('MySQL refused to connect. ' . $conn->connect_error);
-		if (isset($_POST['reqIsAjax']))
-		{
-			$error_data		=	array();
-			$error_data[]	=	'Connection Error';
-			$error_data[]	=	'The server failed to connect. Please retry your request. If the problem persists, please report this error.';
-			echo json_encode($error_data);
-			exit;
-		} else {
-			$title		=	'Connection Error';
-			$message	=	'The server failed to connect. Please retry your request. If the problem persists, please report this error.';
-			alertUser($title, $message);
-			// For a page request, exiting the script is not the appropriate action.
-			exit;
-		}
+		$errorData	=	array(
+							'title'		=>	'Connection Error.',
+							'message'	=>	'The server failed to connect. Please retry your request. If the problem persists, please report this error.',
+							'error'		=>	$conn->connect_error
+						);
+		handleError($errorData, $conn, 'mysql');
 	}
 	return $conn;
 }
 
-// Move this function to the appropriate module
-function getFeaturedAds()
+function db_query($q, &$conn) {
+	$result = $conn->query($q);
+	if (!$result)
+	{
+		$errorData	=	array(
+							'title'		=>	'Database Query Failed.',
+							'message'	=>	'Please retry your request. If the problem persists, please report this error.',
+							'error'		=>	$conn->error
+						);
+		handleError($errorData, $conn, 'mysql');
+	}
+	return $result;
+}
+
+function handleError($errorData, &$conn, $source)
 {
-	$conn	=	Db::getInstance(AL_DB);
+	// array $errorData, obj $conn, string $source
+	// $errorData must contain at least 3 keys: 'title', 'message', and 'error'
+	switch ($source)
+	{
+		case 'mysql':
+			$log	=	LOGS_PATH . '/mysql-errors.log';
+			break;
+		case 'ajax':
+			$log	=	LOGS_PATH . '/ajax-errors.log';
+			break;
+		default:
+			$log	=	LOGS_PATH . '/app-errors.log';
+			break;
+	}
+	ini_set('error_log', $log);
+	error_log($errorData['title'] . ' ' . $errorData['error']);
+	if (isset($_POST['reqIsAjax']))
+	{
+		$result		=	0;
+		$title		=	$errorData['title'];
+		$message	=	$errorData['message'];
+		ajaxResponse_alert($result, $title, $message);
+		exit;
+	} else {
+		alertUser($errorData['title'], $errorData['message']);
+		// For a page request, exiting the script may not be the appropriate action.
+		exit;
+	}
+}
+
+function ajaxResponse_alert($result, $title, $message)
+{
+	// boolean $result, array $alertData
+	$msg	=	array(
+					$result,
+					$title,
+					$message
+				);
+	echo json_encode($msg);
 }
 
 function alertUser($title, $message)
