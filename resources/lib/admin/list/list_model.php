f6fe078bea33a0d21ec4826407a02db1a2df498d
Jack Brown <jack@atlanticlabequipment.com>
2017-05-15 16:16:04 -0400

    Further dev. of the list model.

--- resources/lib/admin/list/list_model.php
+++ resources/lib/admin/list/list_model.php
@@ -7,28 +7,50 @@
 
 class DataList
 {
-	public	$ltype, $lscope;
+	public	$ltype, 
+			$lscope, 
+			$query, 
+			$sortField;
 	
-	private $fieldMap 	=	array(	1	=>	'',
-			2	=>	'',
-			3	=>	'',
-			4	=>	'',
-			5	=>	'',
-			6	=>	'',
-	);
+	private $fieldMap 	=	array();
 	private $fields		=	array();
+	private $conn;
 	
-	public function __construct()
+	public function __construct(&$conn)
 	{
+		/*
+		 * Sets list type, list scope, the search term, and the table fields.
+		 */
+		$this->conn		=	$conn;
+		$this->setFieldMap();
 		$this->setListType();
 		$this->setListScope();
 		$this->setSearchQuery();
 		$this->setFields();
 	}
 	
+	private function setFieldMap()
+	{
+		/*
+		 * Takes no args, simply goes into database to retreive names of necessary fields.
+		 */
+		$q		=	"SELECT id, field_name FROM field_map";
+		$r		=	db_query($q, $this->conn);
+		$count	=	$r->num_rows;
+		for ($j = 0 ; $j < $count ; $j++)
+		{
+			$r->data_seek($j);
+			$row						=	$r->fetch_array(MYSQLI_ASSOC);
+			$this->fieldMap[$row['id']]	=	$row['field_name'];	
+		}
+	}
+	
 	private function setListType()
 	{
-		// Determine which list to generate.
+		/* 
+		 * Determine which list to generate.
+		 * Takes no args, looks for $_GET['ltype'], the URL param for "list type"
+		 */
 		if (isset($_GET['ltype']))
 		{
 			switch ($_GET['ltype'])
@@ -68,7 +90,10 @@ class DataList
 	
 	private function setListScope()
 	{
-		// Determine the scope of the given list.
+		/*
+		 * Set the scope property. This property determines the set of results based on completion/review status.
+		 * Takes no args, looks for $_GET['lscp'], the URL param for 'list scope'
+		 */ 
 		if (isset($_GET['lscp']))
 		{
 			switch ($_GET['lscp'])
@@ -93,7 +118,11 @@ class DataList
 	
 	private function setSearchQuery()
 	{
-		// Set $query as the given input, else set it to NULL if not supplied.
+		/* 
+		 * Sets the query property, which is used to filter results based on the terms given.
+		 * Set $query as the given input, else set it to NULL if not supplied.
+		 * 
+		 */
 		if (isset($_GET['q']))
 		{
 			$this->query	=	$_GET['q'];
@@ -104,6 +133,11 @@ class DataList
 	
 	private function setFields()
 	{
+		/*
+		 * Sets the fields property, which is an array of field names.
+		 * Takes no args, looks for $_GET['f'], which is a comma-separated array of field ids.
+		 * This method takes those ids and maps them to their names, complete with table prefixes.
+		 */
 		if (isset($_GET['f']))
 		{
 			$fields	=	explode(',', $_GET['f']);
@@ -113,6 +147,7 @@ class DataList
 				{
 					$this->fields[]	=	$this->fieldMap[$fieldId];
 				} else {
+					// Given ID did not have map value.
 					// Throw notice? Log warning?
 					continue;
 				}
@@ -122,17 +157,24 @@ class DataList
 	
 	private function setSortField()
 	{
-		// Determine which field to sort by, default to ALE Asset.
+		/*
+		 * Sets the sortField property, which determines the field to sort by.
+		 * Default to ALE Asset is $_GET['srt_f' is not set.
+		 * Given the id of a field, map it to its name.
+		 */
 		if (isset($_GET['srt_f']))
 		{
 			if (in_array($_GET['srt_f'], $this->fieldMap))
 			{
-				
+				$this->sortField	=	$this->fieldMap[$_GET['srt_f']];
 			} else {
-				
+				// Defaults to ALE Asset if given id is invalid, sends error report
+				$this->sortField	=	'itemlist.aleAsset';
+				// THROW NOTICE
 			}
 		} else {
-			
+			// Defaults to ALE Asset if $_GET['srt_f'] is not set.
+			$this->sortField	=	'itemlist.aleAsset';
 		}
 	}
 	
